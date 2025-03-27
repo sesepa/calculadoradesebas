b3f2e9bacedb2442f4827c468c40249aa5b63630
sesepa <sebastianpacanchique552@gmail.com>
2025-03-27 17:45:35 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="calculadora">
+      <div class="pantalla">0</div>
+      <div class="botones">
+        <button class="boton">7</button>
+        <button class="boton">8</button>
+        <button class="boton">9</button>
+        <button class="boton">/</button>
+        <button class="boton">4</button>
+        <button class="boton">5</button>
+        <button class="boton">6</button>
+        <button class="boton">*</button>
+        <button class="boton">1</button>
+        <button class="boton">2</button>
+        <button class="boton">3</button>
+        <button class="boton">-</button>
+        <button class="boton">0</button>
+        <button class="boton">.</button>
+        <button class="boton igual">=</button>
+        <button class="boton">+</button>
+      </div>
+    </div>
+  );
+}
